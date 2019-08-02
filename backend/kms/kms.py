ca3fcd95b5536c6e5dfb3037baef4c1e8438f668
Yuan Rui <njuyuanrui@163.com>
2019-08-02 15:19:34 +0800

    baisc directory struct

---
+++ backend/kms/kms.py
@@ -0,0 +1,35 @@
+# -*- coding:utf-8 -*- 
+#Copyright 2019 TEEX
+#
+#Licensed under the Apache License, Version 2.0 (the "License");
+#you may not use this file except in compliance with the License.
+#You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+#Unless required by applicable law or agreed to in writing, software
+#distributed under the License is distributed on an "AS IS" BASIS,
+#WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#See the License for the specific language governing permissions and
+#limitations under the License.
+
+'''
+A module for to manage secret keys of TEE in two ways:
+1. local encryted storage
+2. remote KMS service           --- the priority
+
+
+
+TODO: 
+Stub Module currently..
+
+Key for test:
+
+addr: 0x71F119Dc662Fd0086c7c90C58e5237d74403a0C0
+pk:   020748e12e9dfc0f83c88eec4528b9a1b1084e80d410158be97953e9980351f254
+sk:   42914050bd0700e2be7c4772b586adb5f3cb5a340caec372a070038086f32891
+
+'''
+
+
+
