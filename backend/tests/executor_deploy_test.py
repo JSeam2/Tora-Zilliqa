2bd97c7743792ed20eaaae9d7b51fec375a078b9
Lili0820 <1179028267@qq.com>
2020-01-17 21:46:48 +0800

    executor tests readme

---
+++ backend/tests/executor_deploy_test.py
@@ -0,0 +1,123 @@
+# -*- coding:utf-8 -*-
+# Copyright 2019 TEEX
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import sys
+import time
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../lib")))
+
+from pprint import pprint
+
+from pyzil.crypto import zilkey
+from pyzil.zilliqa import chain
+from pyzil.account import Account
+from pyzil.contract import Contract
+from pyzil.zilliqa.api import ZilliqaAPI
+
+chain.set_active_chain(chain.TestNet)
+
+# user account
+account = Account(private_key="919457fa2d81c0b7f1f1918683b1ff6b459c444aefec494c92f34d746ebb6b73")
+balance = account.get_balance()
+print("{}: {}".format(account, balance))
+
+
+# oracle_address = '0x' + zilkey.to_valid_address("zil1mejpsqd5cw589xyq3llzvrk0nvetm9v0l5kcn7")
+# pprint(oracle_address)
+
+
+def __has_txn(api, block_num):
+    block_info = api.GetTxBlock(block_num)
+    if block_info["header"]["NumTxns"] == 0:
+        return False
+    else:
+        return True
+
+
+def __get_response_from_block(contract_addr, api, block_num):
+    txns = api.GetTransactionsForTxBlock(block_num)
+    for txn in txns:
+        if len(txn) != 0:
+            receipt = api.GetTransaction(txn[0])["receipt"]
+            if "event_logs" in receipt:
+                event_logs = receipt["event_logs"]
+                for event_log in event_logs:
+                    if event_log["address"] == (zilkey.normalise_address(contract_addr)).lower():
+                        if event_log["_eventname"] == "callback":
+                            print("Get a response...")
+                            print(event_log)
+                            return True
+    return False
+
+
+def get_response_event(contract_addr):
+    url = "https://dev-api.zilliqa.com/"
+    api = ZilliqaAPI(url)
+    cur_block_num = str(int(api.GetCurrentMiniEpoch()) - 10)
+    while True & (int(cur_block_num) != 0):
+        if int(cur_block_num) >= int(api.GetCurrentMiniEpoch()):
+            time.sleep(1)
+        else:
+            if __has_txn(api, cur_block_num):
+                if __get_response_from_block(contract_addr, api, cur_block_num):
+                    break
+                else:
+                    cur_block_num = str(int(cur_block_num) + 1)
+            else:
+                cur_block_num = str(int(cur_block_num) + 1)
+
+
+def deploy_execution_script(exprs):
+    contract_addr = "zil1mejpsqd5cw589xyq3llzvrk0nvetm9v0l5kcn7"
+    contract = Contract.load_from_address(contract_addr)
+    contract.account = account
+    resp = contract.call(method="deployScript", params=[
+        Contract.value_dict('exprs', "String", exprs)], priority=True)
+    if resp['receipt']['success']:
+        event_logs = resp['receipt']['event_logs']
+        if event_logs[0]['_eventname'] == 'Script Deploy':
+            print("Script committed successfully, the script id is " + event_logs[0]['params'][1]['value'])
+        else:
+            print("Commit fail, please see the event log")
+            pprint(event_logs)
+    else:
+        print("Commit fail")
+        pprint(resp)
+
+
+def deploy_test():
+    script = """
+
+x = inp[0] + 1
+y = inp[1] + 1
+
+outp = x + y
+
+    """
+
+    lines = script.split('\n')
+    exprs = []
+    for line in lines:
+        if line != '' and line != '    ' and not line.startswith('#'):
+            exprs.append(line)
+    exprs = str(exprs)
+    print(exprs)
+    deploy_execution_script(exprs)
+
+
+if __name__ == "__main__":
+    deploy_test()
---
+++ docs/Executor.md
@@ -0,0 +1,59 @@
+#### 3. Python Executor
+
+##### Step 0: Commit your script
+
+* You can commit your scripts by invoking the **deployScript** transition in the **ToraGeneral** contract. The example code is in **backend/tests/executor_deploy_test.py**
+
+  * The example script is as the following:
+
+    * **inp** is a reserved word, which is an array composed of the inputs of your script in order.
+    * In the example case, the inputs are **two integers**, the result of the script is the sum of the two integers which are respectively added 1. 
+
+    ```
+    x = inp[0] + 1
+    y = inp[1] + 1
+    
+    outp = x + y
+    ```
+
+  After committing your script successfully, the **scriptId** will be returned to you:
+
+  ```
+  $ python3 executor_deploy_test.py
+  Terminal:
+  ----------
+  Script committed successfully, the script id is 1
+  ```
+
+#####Step 1: Invoke your script
+
+* Write the user contract，the example contract is in **contracts/ExecutorRequest.scilla** which invoking the **executeScript** transition defined in the **ToraGeneral** contract.
+
+    ```
+    transition request()
+        accept;
+        msg = { _tag : "executeScript"; _recipient : oracle_address; _amount : _amount;
+        ...(other params); scriptId: Uint32 1; inputs: "[1, 2]" };
+        msgs = one_msg msg;
+        send msgs
+    end
+    ```
+
+* ExecutorRequest.scilla is for the trial to get the result of the above script defined in **Step 0** with **input** 1 and 2. 
+
+* In addition to  **inputs** required to be designated, you should designate the **scriptId** returned in **Step 0**.
+
+* After deploying your user contract, you can invoke it by the example code in **backend/tests/executor_request_test.py**.
+
+    You can achieve the result as the following:
+
+    ```
+    $ python3 executor_request_test.py
+    Terminal:
+    ----------
+    {'_eventname': 'callback', 'address': '0x0dcca194fa156e388f01b3c4b9993d031d596699', 'params': [{'type': 'String', 'value': '5', 'vname': 'msg'}]}
+    ```
+
+    
+
+    
